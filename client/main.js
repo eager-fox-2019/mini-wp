7514e328dce525777351b5195f2a71e7b45ca1f2
aryadimade <madearyadi@gmail.com>
2019-06-17 18:15:52 +0700

    spertinya udah ok

---
+++ client/main.js
@@ -0,0 +1,17 @@
+$('#content-section-add').hide();
+
+function showAddContainer() {
+  event.preventDefault();
+  $('#content-section-add').show();
+  $('#content-section-article').hide();
+}
+
+function showArticleList() {
+  event.preventDefault();
+  $('#content-section-add').hide();
+  $('#content-section-article').show();
+}
+
+// $('#content-section-article').hide();
+
+
