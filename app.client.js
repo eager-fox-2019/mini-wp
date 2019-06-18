4f19049d5f07be722e8f72d00d88c46626ff97b9
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-18 16:53:13 +0700

    vue

--- app.client.js
+++ app.client.js
@@ -3,30 +3,7 @@ var vueApp = new Vue({
     el: '#app',
     data: {
         articles: [
-            {
-                title: 'test',
-                description: 'lorem'
-            },
-            {
-                title: 'test',
-                description: 'lorem'
-            },
-            {
-                title: 'test',
-                description: 'lorem'
-            },
-            {
-                title: 'test',
-                description: 'lorem'
-            },
-            {
-                title: 'test',
-                description: 'lorem'
-            },
-            {
-                title: 'test',
-                description: 'lorem'
-            },
+            
         ]
     }
 })
--- vue/footer.js
+++ vue/footer.js
@@ -1,4 +1,4 @@
-Vue.component('footer', {
+Vue.component('footer-component', {
     template: `
         <footer>
             <p class="padding-m color-white">MiniWP<br>@hemhem 2020</p>
