5d88d150bc535a5c673ec5479fb5b90d2fe7667e
melinadewi <hds.melina@gmail.com>
2019-06-19 00:55:21 +0700

    Server Updated

--- client/main.js
+++ client/main.js
@@ -4,7 +4,12 @@ var app = new Vue({
         'editor': Editor
     },
     data: {
-        isLogin: true,
+        isLogin: false,
+        usersign: 'login',
+        // newUser: {},
+        userName: '',
+        userEmail: '',
+        userPsw: '',
         loadPage: "published",
         listArticles: [],
         newTitle: "",
@@ -28,35 +33,96 @@ var app = new Vue({
         })
     },
     methods: {
+        // User
+        clearUserForm(){
+            this.userName = ''
+            this.userEmail = ''
+            this.userPsw = ''
+        },
+        toRegister(){
+            this.usersign = 'register'
+            this.clearUserForm
+        },
+        toLogin(){
+            this.usersign = 'login'
+            this.clearUserForm
+        },
+        userRegister(){
+            axios({
+                method: "POST",
+                url: `http://localhost:3000/user/create`,
+                data: {
+                    username: this.userName,
+                    email: this.userEmail,
+                    password: this.userPsw
+                }
+            })
+                .then(() => {
+                    console.log("New Account Created")
+                    this.clearUserForm
+                })
+                .catch(err => {
+                    console.log("Error from userRegister: ", err)
+                })
+        },
+        userLogin(){
+            axios({
+                method: "POST",
+                data: {
+                    email: this.userEmail,
+                    password: this.userPsw
+                },
+                url: `http://localhost:3000/user/login`
+            })
+                .then(function(res) {
+                    localStorage.setItem("token", res.token)
+                    localStorage.setItem("username", res.username)
+                    this.clearUserForm
+                    isLogin = true
+                    console.log("this.isLogin sekarang: ",this.isLogin);
+                    this.usersign = 'logout'
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        userLogout(){
+            localStorage.removeItem("token")
+            localStorage.removeItem("username")
+            googleSignOut()
+            this.isLogin = false
+            this.usersign = 'login'
+        },
+        // Article
         getArticle(){
             axios({
                 method: "GET",
                 url: `http://localhost:3000/article`
             })
-            .then(({data})=>{
-                data.sort(function(a,b){
-                    return new Date(a.updatedAt) - new Date(b.updatedAt);
-                });
-                this.listArticles = data
-            })
-            .catch(err => {
-                console.log("Error from getArticle: ", err)
-            })
+                .then(({data})=>{
+                    data.sort(function(a,b){
+                        return new Date(a.updatedAt) - new Date(b.updatedAt);
+                    });
+                    this.listArticles = data
+                })
+                .catch(err => {
+                    console.log("Error from getArticle: ", err)
+                })
         },
         getPersonalArticle(){
             axios({
                 method: "GET",
                 url: `http://localhost:3000/article/my-post`
             })
-            .then(({data})=>{
-                data.sort(function(a,b){
-                    return new Date(a.updatedAt) - new Date(b.updatedAt);
-                });
-                this.listArticles = data
-            })
-            .catch(err => {
-                console.log("Error from getPersonalArticle: ", err)
-            })
+                .then(({data})=>{
+                    data.sort(function(a,b){
+                        return new Date(a.updatedAt) - new Date(b.updatedAt);
+                    });
+                    this.listArticles = data
+                })
+                .catch(err => {
+                    console.log("Error from getPersonalArticle: ", err)
+                })
         },
         postArticle(){
             axios({
@@ -69,31 +135,31 @@ var app = new Vue({
                     content: this.newPost
                 }
             })
-            .then(() => {
-                console.log("postArticle success")
-                this.getPersonalArticle()
-                this.newTitle = ""
-                this.newImgUrl = ""
-                this.newPost = ""
-                this.loadPage = 'published'
-            })
-            .catch(err => {
-                console.log("Error from postArticle: ", err)
-            })
+                .then(() => {
+                    console.log("postArticle success")
+                    this.getPersonalArticle()
+                    this.newTitle = ""
+                    this.newImgUrl = ""
+                    this.newPost = ""
+                    this.loadPage = 'published'
+                })
+                .catch(err => {
+                    console.log("Error from postArticle: ", err)
+                })
         },
         deleteArticle(id){
             axios({
                 method: "DELETE",
                 url: `http://localhost:3000/article/${id}`
             })
-            .then(() => {
-                console.log("Delete success")
-                this.getPersonalArticle()
-                this.loadPage = 'published'
-            })
-            .catch(err => {
-                console.log("Error from deleteArticle: ", err)
-            })
+                .then(() => {
+                    console.log("Delete success")
+                    this.getPersonalArticle()
+                    this.loadPage = 'published'
+                })
+                .catch(err => {
+                    console.log("Error from deleteArticle: ", err)
+                })
         },
         toDraftPage(){
             this.loadPage = 'draft'
@@ -114,14 +180,14 @@ var app = new Vue({
                 method: "GET",
                 url: `http://localhost:3000/article/${id}`
             })
-            .then(({data})=>{
-                console.log("Get data:", data)
-                this.selectedArticle = data
-                this.loadPage = 'read-more'
-            })
-            .catch(err => {
-                console.log("Error from readMore: ", err)
-            })
+                .then(({data})=>{
+                    console.log("Get data:", data)
+                    this.selectedArticle = data
+                    this.loadPage = 'read-more'
+                })
+                .catch(err => {
+                    console.log("Error from readMore: ", err)
+                })
         },
         shortText(text){
             return text.split(' ').slice(0,10).join(' ')
@@ -131,17 +197,17 @@ var app = new Vue({
                 method: "GET",
                 url: `http://localhost:3000/article/${id}`
             })
-            .then(({data})=>{
-                console.log("Get data:", data)
-                this.newTitle = data.title
-                this.newImgUrl = data.imgUrl
-                this.newPost = data.content
-                this.selectedArticle = data
-                this.loadPage = 'edit-mode'
-            })
-            .catch(err => {
-                console.log("Error from beforeEdit: ", err)
-            })
+                .then(({data})=>{
+                    console.log("Get data:", data)
+                    this.newTitle = data.title
+                    this.newImgUrl = data.imgUrl
+                    this.newPost = data.content
+                    this.selectedArticle = data
+                    this.loadPage = 'edit-mode'
+                })
+                .catch(err => {
+                    console.log("Error from beforeEdit: ", err)
+                })
         },
         afterEdit(id){
             axios({
@@ -153,16 +219,16 @@ var app = new Vue({
                     content: this.newPost
                 }
             })
-            .then(() => {
-                this.getPersonalArticle()
-                this.newTitle = ""
-                this.newImgUrl = ""
-                this.newPost = ""
-                this.loadPage = 'published'
-            })
-            .catch(err => {
-                console.log("Error from afterEdit: ", err)
-            })
+                .then(() => {
+                    this.getPersonalArticle()
+                    this.newTitle = ""
+                    this.newImgUrl = ""
+                    this.newPost = ""
+                    this.loadPage = 'published'
+                })
+                .catch(err => {
+                    console.log("Error from afterEdit: ", err)
+                })
         }
     },
     computed: {
@@ -174,3 +240,26 @@ var app = new Vue({
         }
     }
 })
+
+
+function onSignIn(googleUser) {
+    var idToken = googleUser.getAuthResponse().id_token;
+    axios.post(`http://localhost:3000/user/google`, { idToken:idToken })
+        .then(function({ data }) {
+            // IMPORTANT! Saves the accessToken from server
+            let profile = googleUser.getBasicProfile();
+            localStorage.setItem('token', data.token);
+            localStorage.setItem("username", profile.fullName())
+            this.isLogin = true
+            this.usersign = 'logout'     
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+function googleSignOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User (Google Account) signed out.');
+    });
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController')
 
 router.post('/create', userController.register)
 router.post('/login', userController.login)
+// router.post('/logout', userController.logout)
 router.post('/google', userController.googleLogin)
 router.get('/get', userController.findAll)
 
