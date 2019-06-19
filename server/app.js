e63af3dc39c20ea5ca4131b6f642b944d3237fda
Orvin Savero <orvinsavero@yahoo.com>
2019-06-19 11:55:23 +0700

    vue

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(morgan("dev"));
 app.use("/", routes);
 app.use(errorHandler)
 
-const PORT = 3000;
+const PORT = process.env.PORT|| 3000;
 app.listen(PORT, ()=> {
     console.log(`connected to localhost ${PORT}`);
 });
