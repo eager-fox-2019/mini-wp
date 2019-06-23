09360464d536562ac0980e8ab3026aeb47ebf4cc
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-23 22:32:10 +0700

    add authorization article middleware

--- server/routes/article.route.js
+++ server/routes/article.route.js
@@ -3,11 +3,12 @@ const articleController = require('../controllers/article.controller')
 const multerMemoryStorage = require('../middlewares/multer.memory.storage')
 const gcsUpload = require('../middlewares/google.cloud.bucket.middleware')
 const authMiddleware = require('../middlewares/authentication.middleware')
+const authorizationMiddleware = require('../middlewares/article.autorization.middleware')
 
 router.get('/', articleController.list)
 router.get('/:id', articleController.detail)
 router.post('/', authMiddleware, multerMemoryStorage('image'), gcsUpload, articleController.create)
-router.delete('/:id', authMiddleware, articleController.delete)
-router.patch('/:id', authMiddleware, articleController.update)
+router.delete('/:id', authMiddleware, authorizationMiddleware, articleController.delete)
+router.patch('/:id', authMiddleware, authorizationMiddleware, multerMemoryStorage('image'), gcsUpload, articleController.update)
 
 module.exports = router
