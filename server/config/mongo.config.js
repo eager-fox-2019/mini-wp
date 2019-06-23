131cf36bee1c0ce21b0a2fdc0854bdfb12a8111f
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-23 23:22:10 +0700

    add DB_MONGO_PROD_URL when in production

--- server/config/mongo.config.js
+++ server/config/mongo.config.js
@@ -1,6 +1,6 @@
 let db_url 
 if (process.env.NODE_ENV === 'production') {
-    throw 'belum ada db production'
+    db_url = process.env.DB_MONGO_PROD_URL
 } else {
     db_url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/`
 }
