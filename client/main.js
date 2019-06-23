9331e44bf1ab145639d6ae88bc407971b0f619ab
dedysimamora <dedysimamora627@gmail.com>
2019-06-23 18:09:53 +0700

    prpare for refactor

--- client/main.js
+++ client/main.js
@@ -35,6 +35,13 @@ var app = new Vue({
         wysiwyg: vueWysiwyg.default.component,
       },
     methods : {
+        onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+          },
         getImage(event) {
         this.image = event.target.files[0]
         
