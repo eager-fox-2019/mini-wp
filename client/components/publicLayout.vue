7b47bc9de6a174ffd759af086357f834966e339c
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-23 21:36:37 +0700

    create publicLayout

---
+++ client/components/publicLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="main-page">
+        <sidebar></sidebar>
+        <div id="content" class="flex flex-column justify-content-space-between">
+            <navbar disableButton="true"></navbar>
+            <slot></slot>
+            <footer-component></footer-component>
+        </div>
+      </div>
+</template>
+
+<script>
+  import sidebar from "./sidebar.vue"
+  import navbar from "./navbar.vue"
+  import footerComponent from './footer'
+
+  export default {
+    props:['sidebarOpen'],
+    components: {
+      sidebar,
+      navbar,
+      footerComponent,
+    }
+    
+  }
+</script>
